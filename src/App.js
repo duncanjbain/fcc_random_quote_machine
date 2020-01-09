a5a51878b2e3c18d750391d1c43b3b8963862e51
Duncan Bain <duncan@bain.io>
2020-01-09 17:28:02 +0000

    Corrected comment methods

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import ReactFCCtest from 'react-fcctest';
 
 
-{/* define API URL and the default query which will be random */}
+// define API URL and the default query which will be random
 const API_URL = 'https://api.quotable.io/';
 const DEFAULT_QUERY = 'random';
 
@@ -11,16 +11,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-{/* initialise state, empty quote */}
+//initialise state, empty quote
     this.state = {
       quote: []
     }
-{/* need to bind FetchRandomQuote to this */}
+// need to bind FetchRandomQuote to this
     this.FetchRandomQuote = this.FetchRandomQuote.bind(this);
 
   }
 
 // fetch random quote using API url and default query, uses fetch and promises to get data and passes back to state
+
   FetchRandomQuote = () => {
     fetch(API_URL + DEFAULT_QUERY)
       .then(response => response.json())
@@ -53,8 +54,8 @@ class App extends React.Component {
             <div className="col-lg-6 mx-auto">
               <blockquote className="blockquote bg-white p-5 shadow rounded">
                 <div><i className="fa fa-quote-left"></i></div>
-                <p className="mb-0 mt-2 font-italic" id='text'>{this.state.quote.content}</p> // fetch quote from state
-                <footer className="blockquote-footer pt-4 mt-4 border-top pb-3" id='author'>{this.state.quote.author} // fetch quote author from state
+                <p className="mb-0 mt-2 font-italic" id='text'>{this.state.quote.content}</p> {/* fetch quote from state and render */}
+                <footer className="blockquote-footer pt-4 mt-4 border-top pb-3" id='author'>{this.state.quote.author} {/* fetch quote author from state and render */}
                 </footer>
 
               </blockquote>
@@ -64,12 +65,12 @@ class App extends React.Component {
           <div className="row pb-5">
             <div className="col-lg-6 mx-auto align-middle text-center">
               <a href={`https://twitter.com/intent/tweet?text=` + this.state.quote.content} className="text-info" 
-              id="tweet-quote"><i class="fab fa-twitter fa-lg"></i> Tweet this quote!</a> // add quote to Twitter URL
+              id="tweet-quote"><i class="fab fa-twitter fa-lg"></i> Tweet this quote!</a> {/* add quote to Twitter URL */}
             </div>
           </div>
           <div className="row pb-3">
             <div className="col-lg-6 mx-auto align-middle text-center">
-            // when button is clicked call the FetchRandomQuote function
+            {/* when button is clicked call the FetchRandomQuote function */}
               <button className="btn btn-primary" id="new-quote" onClick={this.FetchRandomQuote}>New Quote!</button> 
             </div>
           </div>
