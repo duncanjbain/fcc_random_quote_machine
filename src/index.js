74aae861a9112341c2484b5902080ba592186539
Duncan Bain <duncan@bain.io>
2020-01-18 00:06:45 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactFCCtest from 'react-fcctest';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
